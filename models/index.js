d49c304b65174f628cb64f77dce95a644ffd02cf
kangsanChang <rkdtks1005@gmail.com>
2017-07-04 00:41:06 +0900

    set sequelize, mongoose 21번 줄 바꿔야 함

---
+++ models/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const mongoConfig = require(__dirname + '/../config/config.json')['mongodb'];
+const mongoose = require('mongoose');
+const db = {};
+const sequelize = config.use_env_variable
+    ? new Sequelize(process.env[config.use_env_variable])
+    : new Sequelize(config.database, config.username, config.password, config);
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoConfig.url);
+db.errandChats = require('./errandChats');
+
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js' && file !== 'errandChats.js');
+    })
+    .forEach(file => {
+        const model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+mongoose.connection.once('open', () => {
+    console.log('Mongoose on!');
+});
+
+module.exports = db;
