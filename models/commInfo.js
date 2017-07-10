588a556c7e7a762d12f10115a2d2f1dec2e5aeb2
SangHyoun <hainco@gmail.com>
2017-07-11 00:13:39 +0900

    commInfoTb 관계 추가

--- models/commInfo.js
+++ models/commInfo.js
@@ -7,24 +7,22 @@ module.exports = (sequelize, DataTypes) => {
         commGrade: {type: DataTypes.INTEGER, field: 'comm_grade'},
         commPhone: {type: DataTypes.STRING(15), field: 'comm_phone', unique: true},
         commBirthday: {type: DataTypes.DATEONLY, field: 'comm_birthday', allowNull: false},
-        commGender: {type: DataTypes.ENUM, values: ['남', '여'], field: 'comm_gender'},
+        commGender: {type: DataTypes.ENUM, values: ['M', 'F'], field: 'comm_gender'},
         commKnownPath: {type: DataTypes.STRING, field: 'comm_known_path'},
         commPictureUrl: {type: DataTypes.TEXT, field: 'comm_picture_url'},
         commPortfolioUrl: {type: DataTypes.TEXT, field: 'comm_portfolio_url'},
         commBlogUrl: {type: DataTypes.TEXT, field: 'comm_blog_url'},
         commPersonalUrl: {type: DataTypes.TEXT, field: 'comm_personal_url'},
         commOtherUrl: {type: DataTypes.TEXT, field: 'comm_other_url'},
-        commUserPosition: {type: DataTypes.ENUM, values: ['개발자', '디자이너'], field: 'comm_user_position'}
+        commUserPosition: {type: DataTypes.ENUM, values: ['designer', 'developer'], field: 'comm_user_position'}
     }, {
         timestamps: true,
         tableName: 'COMM_INFO_TB',
         underscored: true,
         comment: '지원자 정보 테이블',
-        classMethods: {
-            associate: models => {
-                // models.userInfoTb.belongsTo(comm_info_tb, {foreignKey: 'userAuthIdx'});
-            }
-        }
     });
+    comm_info_tb.associate = (models) => {
+        models.userInfoTb.hasOne(comm_info_tb, {foreignKey: 'userAuthIdx'});
+    };
     return comm_info_tb;
 };
