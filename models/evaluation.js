263021f59acc2c18287fae31edf499a904eeccaa
kangsanChang <rkdtks1005@gmail.com>
2017-07-11 12:49:36 +0900

    remove Comment field STRING limit

--- models/evaluation.js
+++ models/evaluation.js
@@ -4,9 +4,9 @@ module.exports = (sequelize, DataTypes) => {
         applicationIdx: {type: DataTypes.INTEGER, field: 'application_idx'},
         interIdx: {type: DataTypes.INTEGER, field: 'inter_idx'},
         evalDocPoint:{type: DataTypes.INTEGER(2), field: 'eval_doc_point'},
-        evalDocComment:{type: DataTypes.STRING(191), field: 'eval_doc_comment'},
+        evalDocComment:{type: DataTypes.STRING, field: 'eval_doc_comment'},
         evalInterviewPoint:{type: DataTypes.INTEGER(2), field: 'eval_interview_point'},
-        evalInterviewComment:{type: DataTypes.STRING(191), field: 'eval_interview_comment'},
+        evalInterviewComment:{type: DataTypes.STRING, field: 'eval_interview_comment'},
         evalIsPass:{type: DataTypes.BOOLEAN, field: 'eval_is_pass'}
     },{
         timestamps: true,
@@ -17,6 +17,6 @@ module.exports = (sequelize, DataTypes) => {
     evaluation_tb.associate = (models) => {
         models.applicationTb.hasMany(evaluation_tb, {foreignKey: 'applicationIdx'});
         models.interviewerTb.hasMany(evaluation_tb, {foreignKey: 'interIdx'});
-    }
+    };
     return evaluation_tb;
-}
+};
