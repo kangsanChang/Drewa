67c54ef54e3c6ebb18852a4d89877af141af1739
SangHyoun <hainco@gmail.com>
2017-07-11 12:40:38 +0900

    협업을 위한 불필요한 자료 정리

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controller/authController');
+const auth = authController.authenticate;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
 module.exports = router;
