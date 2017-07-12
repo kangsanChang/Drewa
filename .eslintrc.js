c915ee6b941b598f005efe93fc0b7e19caaf5d62
SangHyoun <hainco@gmail.com>
2017-07-12 23:04:35 +0900

    require 다음 공뱅행 강요 끔

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,19 @@
 module.exports = {
     'extends': 'airbnb-base',
     'rules': {
+        // 공백 스페이스 갯수
         'indent': [
             'error',
             4,
         ],
+        // 객체 선언시 '{' 뒤, '}' 앞에 스페이스 강요
         'object-curly-spacing': [
             'error',
             'always',
+        ],
+        // require 다음 공백행 강제를 끔
+        'import/newline-after-import': [
+            'off'
         ],
     },
 };
