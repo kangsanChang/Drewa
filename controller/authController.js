dd4303ebb55de68c0bb4c39dc69d34dc31d7ab02
SangHyoun <hainco@gmail.com>
2017-07-27 21:32:35 +0900

    eslint 적용

--- controller/authController.js
+++ controller/authController.js
@@ -1,60 +1,67 @@
-const jwtStrategy = require('passport-jwt').Strategy;
+const JWTStrategy = require('passport-jwt').Strategy;
 const extractJwt = require('passport-jwt').ExtractJwt;
 const config = require('../config/config.json');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const bcrypt = require('bcrypt');
+const models = require('../models');
 
 const jwtPassport = () => {
-    const opts = {
-        /*
-         * extractJwt.fromAuthHeader -> Header name is 'Authorization', values are like 'JWT TOKEN'
-         * extractJwt.fromHeader('NAME') -> Header name is 'NAME', values are like 'TOKEN'
-         */
-        // jwtFromRequest: extractJwt.fromAuthHeader(),
-        jwtFromRequest: extractJwt.fromHeader('token'),
-        secretOrKey: config.auth.SECRET_KEY
-    };
-    passport.use(new jwtStrategy(opts, (jwt_payload, done) => {
-        // TODO: 분석된 토큰 매칭하는 로직 만들기
-        // Matching decoded token
-        if (jwt_payload) {
-            return done(null, jwt_payload)
-        }
-        return done(new Error("No user matched with jwt payload"))
-    }));
+  const opts = {
+    /*
+     * extractJwt.fromAuthHeader -> Header name is 'Authorization', values are like 'JWT TOKEN'
+     * extractJwt.fromHeader('NAME') -> Header name is 'NAME', values are like 'TOKEN'
+     */
+    // jwtFromRequest: extractJwt.fromAuthHeader(),
+    jwtFromRequest: extractJwt.fromHeader('token'),
+    secretOrKey: config.auth.SECRET_KEY,
+  };
+  passport.use(new JWTStrategy(opts, (jwtPayload, done) => {
+    // TODO: 분석된 토큰 매칭하는 로직 만들기
+    // Matching decoded token
+    if (jwtPayload) {
+      return done(null, jwtPayload);
+    }
+    return done(new Error('No user matched with jwt payload'));
+  }));
 };
 
 const comparePassword = async (userEmail, userPassword) => {
-    try {
-        // TODO: findByEmail 함수 완성할것
-        let result = await Users.findByEmail(userEmail);
-        if (!result) {
-            throw new Error('Email not exist');
-        }
-        // Password Matching
-        // TODO: 모델링 완성되면 userPassword 설정할것
-        const isMatch = await bcrypt.compare(userPassword, result.userPassword);
-        if (!isMatch) {
-            throw new Error('Password Not match');
-        }
-    } catch (err) {
-        throw err
+  try {
+    // TODO: findByEmail 함수 완성할것
+    // const result = await Users.findByEmail(userEmail);
+    const condition = {
+      where: {
+        userEmail,
+      },
+    };
+    const result = await models.userInfoTb.find(condition);
+    if (!result) {
+      throw new Error('Email not exist');
     }
+    // Password Matching
+    // TODO: 모델링 완성되면 userPassword 설정할것
+    const isMatch = await bcrypt.compare(userPassword, result.userPassword);
+    if (!isMatch) {
+      throw new Error('Password Not match');
+    }
+  } catch (err) {
+    throw err;
+  }
 };
 
 const createToken = async (userEmail, userName, userType) => {
-    const payloads = {
-        userEmail: userEmail,
-        userName: userName,
-        userType: userType
-    };
-    const token = await jwt.sign(payloads, config.auth.SECRET_KEY, {expiresIn: config.auth.EXPIRES});
-    return token;
+  const payloads = {
+    userEmail,
+    userName,
+    userType,
+  };
+  const token = await jwt.sign(payloads, config.auth.SECRET_KEY,
+    { expiresIn: config.auth.EXPIRES });
+  return token;
 };
 
-const authenticate = passport.authenticate('jwt', {session: false});
-
+const authenticate = passport.authenticate('jwt', { session: false });
 
 module.exports.jwtPassport = jwtPassport;
 module.exports.comparePassword = comparePassword;
