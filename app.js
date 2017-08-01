11a2cfc174e4a0cd1c561a6f5926a9197b315d55
SangHyoun <hainco@gmail.com>
2017-08-01 16:33:39 +0900

    Error handler with console.log

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({ msg: err.message, data: null });
+  console.log(err);
 });
 
 module.exports = app;
