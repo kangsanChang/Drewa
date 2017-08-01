fbff834d960eeea7d6287f3a33f1adf1d68875ec
kangsan <rkdtks1005@gmail.com>
2017-08-01 17:01:40 +0900

    fix small errors

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   res.status(err.status || 500);
   res.json({ msg: err.message, data: null });
 });
--- controller/applicant.js
+++ controller/applicant.js
@@ -49,7 +49,7 @@ module.exports.postSignUp = async (req, res, next) => {
     await t.commit();
     res.json(result);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     next(err);
   }
 };
