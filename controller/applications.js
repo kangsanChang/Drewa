4301c94ed31752ec7f3bcc0124273cce10fb1f48
kangsan <rkdtks1005@gmail.com>
2017-08-04 14:48:37 +0900

    Get userAuthIdx from userEamil, convert String to Date in each value of interviewAvailableTime array

--- controller/applications.js
+++ controller/applications.js
@@ -25,23 +25,28 @@ module.exports.postApplications = async (req, res, next) => {
     };
     // json array 로 받는 애들 비워졌을 경우에 어떻게 처리할 지 프론트에 정의 되어 있어야 함
 
+    // 토큰에서 user email 가져오는 부분 필요...
+    // CODE ...
+    // 헤더에서 토큰 까서 email 가지고 왔다(tempEmail)고 생각하고 진행.
+    const tempEmail = 'test7@gmail.com';
     // 현재는 수동으로 userAuthIdx 지정해 주어야 함. 토큰에서 까서 찾아내는 코드 필요
     const appDocData = {
       cardinalNumber: 3,
-      userAuthIdx: 13,
+      userAuthIdx: null,
       algorithmAnswer: data.algorithmAnswer,
       interviewAvailableTime: data.interviewAvailableTime,
       answers: data.answers,
     };
 
-    // console.log('*******************************');
-    // console.log('userName : ', userName, '\ncommData : ', commData, '\nappDocData : ', appDocData);
-    // console.log('*******************************');
-
-    // 토큰에서 user email 가져오는 부분 필요...
-    // CODE ...
-    // 헤더에서 토큰 까서 email 가지고 왔다(tempEmail)고 생각하고 진행.
-    const tempEmail = 'test1@gmail.com';
+    await models.userInfoTb.findOne({ where: { userEmail: tempEmail } })
+                .then((result) => {
+                  // userAuthIdx 가져오기
+                  appDocData.userAuthIdx = result.dataValues.userAuthIdx;
+                  // interviewAvailavleTime 의 배열 요소들을 Date 타입으로 변경
+                  // 몽고디비 확인해보면 string으로 넣어도 알아서 Date 타입으로 변경해서 저장하는 것 같긴 함
+                  appDocData.interviewAvailableTime
+                            .forEach((elem, i, arr) => { arr[i] = new Date(elem); });
+                });
 
     // DB에 넣어주기
     const userInfoResult = await models.userInfoTb.update({ userName },
@@ -59,7 +64,6 @@ module.exports.postApplications = async (req, res, next) => {
   }
 };
 
-/*
 module.exports.getApplications = async (req, res, next) => {
   try {
     const result = await models.applicationDoc.find().exec();
@@ -85,4 +89,3 @@ module.exports.getApplication = async (req, res, next) => {
     next(err);
   }
 };
-*/
